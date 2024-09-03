61c07b346a6e2c230b33757ba83cd068d8b00c81
pratiyankkumar <kumarpratiyank@gmail.com>
2024-09-03 12:30:48 +0530

    Added delete comment option

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -156,6 +156,13 @@ document.addEventListener('DOMContentLoaded', async () => {
     
               <button class="bg-button font-bold w-20 py-2 mt-3 rounded-full js-comment-post-button">Post</button>
             </div>
+
+            <div class="w-72 md:h-16 h-14 bg-red-500 shadow-red-400 shadow-sm fixed right-16 top-10 flex flex-row items-center rounded-full js-error-message hidden">
+              <svg class="ml-4 size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+              </svg>
+              <p class="ml-3">You can't delete this comment!</p>      
+            </div>
           </div>
         `;
 
@@ -174,13 +181,20 @@ document.addEventListener('DOMContentLoaded', async () => {
         for (const comment of post.comments) {
           const commentOwner = await findUser(comment.user)
           commentHTML += `
-            <div class="flex flex-row items-center mt-3">
-              <div class="flex items-center justify-center bg-gray-500 rounded-full size-10">
-                ${commentOwner[0].toUpperCase()}
+            <div class="flex flex-row items-center">
+              <div class="flex flex-row items-center mt-3">
+                <div class="flex items-center justify-center bg-gray-500 rounded-full size-10">
+                  ${commentOwner[0].toUpperCase()}
+                </div>
+                <div class="px-4 py-2 bg-gray-600 rounded-full ml-3">
+                  <p>${comment.comment}</p>
+                </div>
               </div>
-              <div class="px-4 py-2 bg-gray-600 rounded-full ml-3">
-                <p>${comment.comment}</p>
-              </div>
+
+              <svg class="size-5 ml-6 js-comment-delete-button cursor-pointer" data-comment-Id="${comment._id}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+              </svg>
+
             </div>
           `;
         }
@@ -210,6 +224,36 @@ document.addEventListener('DOMContentLoaded', async () => {
           generatePcFeed()
           document.querySelector('.js-pc-feed').innerHTML = feedHtml;
         });
+
+        document.querySelectorAll('.js-comment-delete-button').forEach((deleteButton) => {
+          deleteButton.addEventListener('click', async () => {
+            try {
+              const commentId = deleteButton.dataset.commentId
+            
+              const request = await fetch(`http://localhost:3000/posts/${id}/comment/${commentId}`, {
+                method: "DELETE",
+                headers: {
+                  'Authorization': `Bearer ${token}`,
+                  'Content-Type': 'application/json'
+                }
+              })
+  
+              const response = await request.json()
+  
+              if (request.status === 200) {
+                console.log(response)
+                window.location.reload()
+              } else {
+                document.querySelector('.js-error-message').classList.remove('hidden')
+                setTimeout(() => {
+                  document.querySelector('.js-error-message').classList.add('hidden')
+                }, 2000);
+              }             
+            } catch (error) {
+              console.log(error)
+            }
+          })
+        })
       });
     });    
   }
